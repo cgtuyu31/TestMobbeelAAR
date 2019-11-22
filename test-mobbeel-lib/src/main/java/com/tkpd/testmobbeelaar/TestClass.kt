ef7ad31a2ed0f11ac1465d72de4271b7e38aefab
Christian Glenn Tuyu <christian.tuyu@tokopedia.com>
2019-11-22 13:21:30 +0700

    init

--- test-mobbeel-lib/src/main/java/com/tkpd/testmobbeelaar/TestClass.kt
+++ test-mobbeel-lib/src/main/java/com/tkpd/testmobbeelaar/TestClass.kt
@@ -1,15 +1,12 @@
 package com.tkpd.testmobbeelaar
 
 import com.tkpd.internal_android_library.InternalAndroidLibrary
-import com.tkpd.testlibraryaar.ExternalMobbeelAndroidLibrary
 
 class TestClass {
-    val lib = InternalAndroidLibrary()
-    val extLib = ExternalMobbeelAndroidLibrary()
+    private val lib = InternalAndroidLibrary()
 
     private fun printLib(): String{
         return "Test MainApp is Using Library :\n" +
-                "${lib.getNested()}\n"+
-                "[${extLib.TAG}]"
+                "${lib.getNested()}\n"
     }
 }
