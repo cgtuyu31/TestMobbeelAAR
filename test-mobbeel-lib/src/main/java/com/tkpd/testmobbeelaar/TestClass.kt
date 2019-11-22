ef45c1218114ef4db0cf6053d1b889d5998d9fdd
Christian Glenn Tuyu <christian.tuyu@tokopedia.com>
2019-11-22 13:50:22 +0700

    changed internal-android-library dependencies to api

--- test-mobbeel-lib/src/main/java/com/tkpd/testmobbeelaar/TestClass.kt
+++ test-mobbeel-lib/src/main/java/com/tkpd/testmobbeelaar/TestClass.kt
@@ -3,9 +3,9 @@ package com.tkpd.testmobbeelaar
 import com.tkpd.internal_android_library.InternalAndroidLibrary
 
 class TestClass {
-    private val lib = InternalAndroidLibrary()
+    val lib = InternalAndroidLibrary()
 
-    private fun printLib(): String{
+    fun printLib(): String{
         return "Test MainApp is Using Library :\n" +
                 "${lib.getNested()}\n"
     }
